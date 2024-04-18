c9ab71563d28af8f51600e49cdfd5a5fdd973dd9
UshaSuwal <usaasuwa@gmail.com>
2024-04-18 14:18:03 +0545

    switch case maintained

--- src/redux/reducers/movieReducers.js
+++ src/redux/reducers/movieReducers.js
@@ -5,20 +5,27 @@ const initialState = {
 };
 
 const movieReducer = (state = initialState, action) => {
-  if (action.type === FETCH_MOVIE_REQUEST) {
-    return { ...state, movie: action.payload };
-  } else if (action.type === ADD_REVIEW) {
-    return {
-      ...state,
-      movie: state.movie.map((m) =>
-        m.mid === action.payload.id
-          ? {
-              ...m,
-              reviews: [...m.reviews, action.payload],
-            }
-          : m
-      ),
-    };
+
+  switch(action.type){
+    case FETCH_MOVIE_REQUEST :
+
+      return { ...state, movie: action.payload };
+      
+    
+    case ADD_REVIEW :
+      return {
+        ...state,
+        movie: state.movie.map((m) =>
+          m.mid === action.payload.id
+            ? {
+                ...m,
+                reviews: [...m.reviews, action.payload],
+              }
+            : m
+        ),
+      };
   }
+
+
 };
 export default movieReducer;
